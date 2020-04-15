de607eb4b1f7f6d5ae05b75cffcba562bd5f17dd
Yoom Davidson <ddavidson202020@gmail.com>
2020-04-15 18:46:47 -0400

    Done with db folder

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Happy Burger", true);
+
+INSERT INTO burgers (burger_name) VALUES ("King Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Burger");
